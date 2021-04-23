1f50ec19f67338a1e1cd82a6c90159263576019f
zakywtf <novazaky1@gmail.com>
2021-04-23 13:21:23 +0700

    feat: initial commit

--- components/layout.js
+++ components/layout.js
@@ -4,8 +4,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '[Your Name]'
-export const siteTitle = 'Next.js Sample Website'
+const name = 'Nova Zaky Fathoni'
+export const siteTitle = 'Personal Website'
 
 export default function Layout({ children, home }) {
   return (
